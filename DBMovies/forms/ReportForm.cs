6a323b016e3af149cca75c6406114533963b2d81
Jiří Hladík <hladas250@email.cz>
2021-03-29 08:00:46 +0200

    Oprava větvení metod

--- DBMovies/forms/ReportForm.cs
+++ DBMovies/forms/ReportForm.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
+
 
 namespace DBMovies
 {
@@ -21,17 +23,21 @@ namespace DBMovies
 
         private void btnSend_Click(object sender, EventArgs e)
         {
-            if (rtbMessage.Text != "")
+            foreach (Window window in System.Windows.Application.Current.Windows)
             {
-                // TODO
-                if (recipient == "admin")
-                    // Předává ze současného formuláře do metody Hlavního okna
-                    ((MovieWindow)System.Windows.Application.Current.MainWindow).registerReport(rtbMessage.Text);
-                else if (recipient == "movie")
-                    ((MovieWindow)System.Windows.Application.Current.MainWindow).registerComment(rtbMessage.Text);
+                if (window.GetType() == typeof(MovieWindow))
+                {
+                    if (rtbMessage.Text != "")
+                    {
+                        if (recipient == "admin")
+                            (window as MovieWindow).registerReport(rtbMessage.Text);
+                        else if (recipient == "movie")
+                            (window as MovieWindow).registerComment(rtbMessage.Text);
+                    }
+                    else 
+                        System.Windows.MessageBox.Show("Message is incomplete.\nPlease try again.", "FAIL");
+                }
             }
-            else
-                MessageBox.Show("Message is incomplete.\nPlease try again.", "FAIL");
         }
     }
 }
